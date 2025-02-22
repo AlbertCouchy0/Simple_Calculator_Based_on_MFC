421f07de27d375bfaac22314c4099ad84ca4f431
zy <129528699+AlbertCouchy0@users.noreply.github.com>
2025-02-22 22:37:37 +0800

    main codes and app

---
+++ calculator/resource.h
@@ -0,0 +1,70 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 calculator.rc 使用
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_CALCULATOR_DIALOG           102
+#define IDI_ICON1                       131
+#define IDR_MAINFRAME                   131
+#define IDD_DIALOG1                     132
+#define IDB_BITMAP1                     134
+#define IDC_EDIT1                       1001
+#define IDC_BUTTON1                     1002
+#define IDC_BUTTON2                     1003
+#define IDC_BUTTON3                     1004
+#define IDC_BUTTON4                     1005
+#define IDC_BUTTON5                     1006
+#define IDC_BUTTON6                     1007
+#define IDC_BUTTONDEL                   1008
+#define IDC_BUTTON8                     1009
+#define IDC_BUTTONAC                    1010
+#define IDC_BUTTONmultiply              1011
+#define IDC_BUTTONdivide                1012
+#define IDC_BUTTONplus                  1013
+#define IDC_BUTTONminus                 1014
+#define IDC_BUTTONANS                   1015
+#define IDC_BUTTON7                     1016
+#define IDC_BUTTON9                     1017
+#define IDC_BUTTON0                     1018
+#define IDC_BUTTONdot                   1019
+#define IDC_BUTTONequal                 1020
+#define IDC_BUTTONsin                   1021
+#define IDC_BUTTONcos                   1022
+#define IDC_BUTTONtan                   1023
+#define IDC_BUTTONcot                   1024
+#define IDC_BUTTONarcsin                1025
+#define IDC_BUTTONarccos                1026
+#define IDC_BUTTONarctan                1027
+#define IDC_BUTTONreciprocal            1028
+#define IDC_BUTTONsquare_root           1029
+#define IDC_BUTTONcube_root             1030
+#define IDC_BUTTONsquare                1031
+#define IDC_BUTTONcube                  1032
+#define IDC_BUTTONpower                 1033
+#define IDC_BUTTONlog                   1034
+#define IDC_BUTTONlog10                 1035
+#define IDC_BUTTONln                    1036
+#define IDC_BUTTONleftparenthese        1037
+#define IDC_BUTTONrightparenthese       1038
+#define IDC_BUTTONe                     1039
+#define IDC_BUTTONpai                   1040
+#define IDC_BUTTONe2                    1040
+#define IDC_BUTTONabout                 1041
+#define IDC_EDIT2                       1042
+#define IDC_BUTTON10                    1044
+#define IDC_TANCHUANG                   1045
+#define IDC_TITLE                       1046
+
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        135
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1047
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
